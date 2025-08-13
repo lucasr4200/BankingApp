7e290389359b1d88a580a95b12059df8e867202e
Lucas Rasmusson <lucasr4200@gmail.com>
2025-08-13 09:15:23 -0600

    Initial Commit

---
+++ BankingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankingApp
+//
+//  Created by Lucas Rasmusson on 2025-08-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BankingAppTests/BankingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BankingAppTests.swift
+//  BankingAppTests
+//
+//  Created by Lucas Rasmusson on 2025-08-13.
+//
+
+import Testing
+@testable import BankingApp
+
+struct BankingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
