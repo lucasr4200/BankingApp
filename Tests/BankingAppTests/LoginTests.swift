13bf5c46e7fe74e2b1ff5900e282ebf15c0c2c71
Lucas Rasmusson <lucasr4200@gmail.com>
2025-09-08 10:05:38 -0600

    REFACTOR
    Added further testing comments for documentation and learning purposes

--- Tests/BankingAppTests/LoginTests.swift
+++ Tests/BankingAppTests/LoginTests.swift
@@ -13,7 +13,7 @@ struct LoginTests {
     // MARK: - Helpers
         private func makeSUT() -> LoginViewController {
             let vc = LoginViewController()
-            vc.loadViewIfNeeded() //triggers viewDidLoad -> style() / layout()
+            vc.loadViewIfNeeded() //triggers viewDidLoad -> style() /layout() (helper functions made)
             return vc
         }
 
@@ -22,6 +22,7 @@ struct LoginTests {
             sut.loginView.passwordTextField.text = pass
         }
 
+    
         // MARK: - Tests
 
         //tests if the initial state is that the error message is hidden
@@ -32,19 +33,20 @@ struct LoginTests {
             #expect(sut.errorMessageLabel.text == nil) // asserts no message is preloaded
         }
 
-        //
+        //tests if the correct error message is shown and that it is correct when username and password are both empty and the sign in button is tapped
         @Test @MainActor
         func emptyUsernameOrPassword_showsRequiredError_andNoSpinner() async throws {
             let sut = makeSUT() // creates a LoginViewController and loads its view
             fill(sut, user: "", pass: "") //set both fields to empty strings
 
-            sut.signInTapped(sender: sut.signInButton) //tape sign in button
+            sut.signInTapped(sender: sut.signInButton) //tap sign in button
 
             #expect(sut.errorMessageLabel.isHidden == false) //assert that the error message is shown
             #expect(sut.errorMessageLabel.text == "Username and password are required") //assert the error message text is correct
             #expect(sut.signInButton.configuration?.showsActivityIndicator != true) // assert activity indicator is not true
         }
 
+        //tests if the error message is shown and that it is correct if the wrong username and passsword are given
         @Test @MainActor
         func incorrectCredentials_showsIncorrectError_andNoSpinner() async throws {
             let sut = makeSUT()
@@ -57,6 +59,8 @@ struct LoginTests {
             #expect(sut.signInButton.configuration?.showsActivityIndicator != true)
         }
 
+        //tests that the error message is hidden and that the activity indicator is succefully shown when sign in button is
+        //tapped and the username and password are correct
         @Test @MainActor
         func validCredentials_showsSpinner_andHidesError() async throws {
             let sut = makeSUT()
